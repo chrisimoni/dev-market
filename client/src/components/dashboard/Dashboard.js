e47b35f7722da9b375622ac9a42e27f2b390f271
chrisimoni <imonireal@gmail.com>
2019-01-07 16:28:11 +0100

    implement createProfile component route

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -28,8 +28,7 @@ class Dashboard extends Component {
         dashboardContent = (
           <div>
             <p className="lead text-muted">Welcome { user.name } </p>
-            <small>You have not yet setup a profile, please add some info</small>
-            <br />
+            <p className="lead text-left">You have not yet setup a profile, please add some info</p>
             <Link to="/create-profile" className="btn btn-lg btn-info"> Creat Profile </Link>
           </div>
         )
