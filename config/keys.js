84622b0cec34d78646c427cc4bb961cb4f4ba5ac
Imoni Chris <imonireal@gmail.com>
2019-01-17 22:52:28 +0100

    implement ProfileAction component and DeleteAccount

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/mern-stack',
+    //mongoURI: 'mongodb://localhost:27017/mern-stack',
+    mongoURI: 'mongodb://chris:abc123@ds157574.mlab.com:57574/dev-market',
     secretOrKey: 'secret' 
 }
